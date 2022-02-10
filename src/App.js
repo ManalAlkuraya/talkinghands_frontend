9365d7ca12ace23cffe82ce1fdb19a9867ab092c
ManalAlkuraya <92294514+ManalAlkuraya@users.noreply.github.com>
2022-02-11 02:33:54 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Outlet, Link } from "react-router-dom";
 import axios from "axios";
 import '../src/component/homepage.css';
-import { useState } from "react/cjs/react.development";
+import { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import React from "react";
 
